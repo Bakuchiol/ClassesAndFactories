6b1db8978cb3ae00900ab79eaa18f063e4006060
Lynette <lynettecargo@Lynettes-Air.home>
2023-06-06 16:52:11 -0500

    creating story

--- homework.js
+++ homework.js
@@ -22,7 +22,7 @@ class Hamster {
 
 //Person
 class Person {
-    constructor(name, age, height, weight, mood, hamsters, bankAccount){
+    constructor(name){
         this.name = name
         this.age = 0
         this.weight = 0
@@ -33,8 +33,8 @@ class Person {
     getName(){
         return this.name
     }
-    getAge(){
-        return this.age
+    getAge(num){
+        return this.age =+ num;
     }
     getWeight(){
         return this.weight
@@ -42,31 +42,46 @@ class Person {
     greet(){
         console.log(`Hello ${this.name}!`)
     }
-    eat(){
+    eat(num){
         //increment weight, increment mood
         // everytime weight goes up, mood goes up
-        this.weight++
-        this.mood++
-        
-    }
-    exercise(){
+        if(this.weight += num){
+            return this.mood += num
+        } else {
+            return this.weight += num
+        }
+    }    
+
+    exercise(num){
         //decrement weight
-        this.weight--
+        return this.weight -= num;
     }
-    ageUp(){
+    ageUp(num){
         //increment age, increment height, increment weight, decrement mood
         // increment bank account by 10 (birthday money)
-        this.age++
-        this.height++
-        this.weight++
-        this.mood--
-        this.bankAccount += 10;
+        // this.age += num;
+        // this.height += num;
+        // this.weight += num;
+        // this.mood -= num;
+        // this.bankAccount += 10 * num;
+       while(this.age += num){}
     }
     buyHamster(hamster){
         //push hamster object onto hamster array
         //increment mood by 10, decrement bankAccount by value of hamster (use getPrice)
-        this.hamsters.push(hamster)
-        this.mood + 10;
-        this.bankAccount - getPrice()
+        this.hamsters.push(hamster);
+        this.mood += 10;
+        this.bankAccount -= hamster.getPrice();
     }
 }
+
+const block = new Person("Timmy")
+console.log(block.getName());
+console.log(block.getAge(5));
+console.log(block.eat(5));
+console.log(block.exercise(5));
+
+console.log(block.getAge(9));
+
+const ham =  new Hamster("", "Gus")
+console.log(ham.owner("Timmy"))
